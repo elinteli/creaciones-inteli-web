bf4fe45184d21665b032ba97d1944a9fc17e1a70
elinteli <114112311+elinteli@users.noreply.github.com>
2022-11-12 13:19:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function AbrirCerrarResponsiveNav() {
+    let Nav = document.getElementById("Nav");
+    let IconMobileNav = document.getElementById("iconomobilenav");
+    if (Nav.className === "nav") {
+      Nav.className += " responsive";
+      IconMobileNav.className = "fa-solid fa-x";
+    } else {
+      Nav.className = "nav";
+      IconMobileNav.className = "fa-solid fa-bars";
+    }
+}
